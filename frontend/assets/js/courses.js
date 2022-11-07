11c588521e6bb07de7ad8d3405175ca9c83ef019
frj0 <touskekunn@gmail.com>
2022-11-07 17:25:21 -0300

    formulario de registro en seccion cursos

---
+++ frontend/assets/js/courses.js
@@ -0,0 +1,31 @@
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Corrobore su nombre");
+    return false;
+  }
+}
+
+function validateForm() {
+  let x = document.forms["myForm"]["fapellido"].value;
+  if (x == "") {
+    alert("Corrobore su e-mail");
+    return false;
+  }
+}
+
+function validateForm() {
+  let x = document.forms["myForm"]["fdni"].value;
+  if (x == "") {
+    alert("Corrobore su DNI");
+    return false;
+  }
+}
+
+function validateForm() {
+  let x = document.forms["myForm"]["femail"].value;
+  if (x == "") {
+    alert("Corrobore su e-mail");
+    return false;
+  }
+}
